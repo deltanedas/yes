db5c45aff813867b100ecb7cba1b697499fa92be
DeltaNedas <deltanedas@gmail.com>
2020-12-04 18:12:30 +0000

    mobile support and update to v120

--- scripts/main.js
+++ scripts/main.js
@@ -8,21 +8,26 @@ Events.on(ClientLoadEvent, () => {
 		const dialog = Core.scene.dialog;
 		const table = dialog.cont.cells.get(0).get();
 		const gh = table.cells.get(1).get();
+
+		// Force the use of the pc dialog which can be hijacked
+		const mobile = Vars.mobile;
+		Vars.mobile = false;
 		gh.fireClick();
 		dialog.hide();
+
 		Core.app.post(() => {
-			if (Vars.mobile) {
-				// TODO: somehow hijack the input listener
-			} else {
-				// hijack the input dialog
+			Core.app.post(() => {
 				Core.app.post(() => {
+					const dialog = Core.scene.dialog;
+					dialog.cont.cells.get(1).get().text = "DeltaNedas/no";
+					dialog.buttons.cells.get(1).get().fireClick();
+					// hide ugly loading dialog
 					Core.app.post(() => {
-						const dialog = Core.scene.dialog;
-						dialog.cont.cells.get(1).get().text = "DeltaNedas/no";
-						dialog.buttons.cells.get(0).get().fireClick();
+						ui.loadfrag.hide();
+						Vars.mobile = mobile;
 					});
 				});
-			}
+			});
 		});
 	});
 });
